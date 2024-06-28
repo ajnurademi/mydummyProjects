96304827966f9fef0ddffaaf37071f24bde82063
ajnurxhaki <ajnur.ademi@hotmail.com>
2024-06-28 18:09:30 +0200

    added styling

--- myExpanseTracker/src/components/expanseTracker/expanseTracker.jsx
+++ myExpanseTracker/src/components/expanseTracker/expanseTracker.jsx
@@ -3,27 +3,38 @@ function expanseTracker() {
 
   return (
     <>
-      <div className="container">
-        <div className="text-3xl font-bold underline">
-          <label className="text-3xl font-bold underline">Expanse Tracker</label>
+      <div className="container mx-auto p-4 bg-white rounded shadow-lg">
+        <div className="mb-4">
+          <label className="text-2xl font-bold tracking-wide text-green-600">Expanse Tracker</label>
         </div>
-        <div className="formular">
-          <p>Add a New Item:</p>
-          <label className="text-3xl font-bold underline">Name:</label>
-          <input type="text" placeholder="Where was the expanse made?" />
-          <label>Date</label>
-          <input type="date" />
-          <label>Amount:</label>
-          <input type="number"/>
+        <div className="formular flex flex-col sm:flex-row items-center mb-4 space-y-4 sm:space-y-0 sm:space-x-4">
+          <p className="font-medium ">Add a New Item:</p>
+          <div className="flex flex-col ">
+            <label className="mb-1">Name:</label>
+            <input
+              type="text"
+              placeholder="Where was the expanse made?"
+              className="p-2 border rounded min-w-80"
+            />
+          </div>
+          <div className="flex flex-col">
+            <label className="mb-1">Date:</label>
+            <input type="date" className="p-2 border rounded" />
+          </div>
+          <div className="flex flex-col">
+            <label className="mb-1">Amount:</label>
+            <input type="number" className="p-2 border rounded" />
+          </div>
         </div>
-        <div className="btn">
-          <button type="submit">Add</button>
+        <div className="btn mb-4">
+          <button type="submit" className="px-4 py-2 bg-green-600 text-white rounded hover:bg-green-700 ">
+            Add
+          </button>
         </div>
         <div className="table">
-          <ExpanseTable/>
+          <ExpanseTable />
         </div>
       </div>
-
     </>
   )
 }
